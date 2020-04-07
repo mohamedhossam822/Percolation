49d817bfb71a65c604a3ceb922d3c3625cd86cb9
Mohamed Hossam <mohamedhossam822@gmail.com>
2020-04-07 19:14:04 +0200

    Initializing

---
+++ src/Percolation.java
@@ -0,0 +1,38 @@
+
+public class Percolation {
+	
+	 public Percolation(int n) {
+		 
+	 }
+
+	    // opens the site (row, col) if it is not open already
+	 public void open(int row, int col) {
+		 
+	 }
+
+	    // is the site (row, col) open?
+	 public boolean isOpen(int row, int col) {
+		 
+	 }
+
+	    // is the site (row, col) full?
+	 public boolean isFull(int row, int col) {
+		 
+	 }
+
+	    // returns the number of open sites
+	 public int numberOfOpenSites() {
+		 
+	 }
+
+	    // does the system percolate?
+	 public boolean percolates() {
+		 
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,30 @@
+
+public class PercolationStats {
+	
+    public PercolationStats(int n, int trials) {
+    	
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+    	
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+    	
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+    	
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+    	
+    }
+
+	public static void main(String[] args) {}
+
+}
